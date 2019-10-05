e2929cb43b5e184fbc38bf837e110a2a81089fb0
Thordur Thordarson <thor@runesmith.org>
2019-10-04 23:52:07 -0400

    Changed the client side code to hydrate the DOM element instead of rendering to it (as its pre-rendered by the server)

--- src/client/startup.js
+++ src/client/startup.js
@@ -1,6 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-
+import { hydrate } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/app';
+import rootReducer from './state/reducers/root';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Grab the state from a global variable injected into the server-generated HTML
+const preloadedState = window.__PRELOADED_STATE__
+
+// Allow the passed state to be garbage-collected
+delete window.__PRELOADED_STATE__
+
+// Create Redux store with initial state
+const store = createStore(rootReducer, preloadedState)
+
+hydrate(<Provider store={store}><App /></Provider>, document.getElementById('root'));
